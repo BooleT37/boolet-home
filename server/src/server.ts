6160b80b1fb03bb238ed03af8b18bb4a5709632c
Алексей Левин <BooleT37@gmail.com>
2018-10-14 13:53:48 +0500

    proxy to get games

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,7 @@ import "./configSettingsInitializer";
 import * as config from "config";
 import * as express from "express";
 import * as fallback from "express-history-api-fallback";
+import * as proxy from "express-http-proxy";
 
 import "./aliasesSetup";
 
@@ -14,7 +15,14 @@ const app = express();
 app.use("/", express.static(clientDistPath));
 app.use(fallback("index.html", { root: clientDistPath }));
 
-console.log(config.get("gamesAssistant.steamApiKey"));
+app.use("/getGames", proxy("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/",
+{
+    proxyReqPathResolver: (req: express.Request) => `${req.url}&key=${config.get<string>("gamesAssistant.steamApiKey")}&format=json`,
+    userResDecorator: (proxyRes, proxyResData) => {
+        const data = JSON.parse(proxyResData.toString());
+        return JSON.stringify(data.response.games.map(g => g.appid));
+      }
+}));
 
 app.listen(port, () => {
     console.log(`Server running on ${port}!`);
