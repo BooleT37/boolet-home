bef81ff64a9a7387a5f4de66616b5a349a74dceb
Алексей Левин <BooleT37@gmail.com>
2018-10-14 12:51:43 +0500

    remove unused imports

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,13 +3,12 @@ import * as fallback from "express-history-api-fallback";
 
 import "./aliasesSetup";
 
-import { clientDistPath, serverPublicPath } from "./paths";
+import { clientDistPath } from "./paths";
 
 const port = process.env.PORT || 8000;
 const app = express();
 
 app.use("/", express.static(clientDistPath));
-// app.use(express.static(serverPublicPath));
 app.use(fallback("index.html", { root: clientDistPath }));
 
 app.listen(port, () => {
